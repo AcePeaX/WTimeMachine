ed1fad1c4a355bfaacbf1e6be9024af19c672385
AcePeaX <benmansour.mohammed2002@gmail.com>
2025-04-03 13:01:51 +0200

    Proy change in Frontend

---
+++ packages/frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api', // Proxy requests starting with "/api"
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Backend URL
+      changeOrigin: true, // Changes the origin of the host header to the target URL
+    })
+  );
+};
